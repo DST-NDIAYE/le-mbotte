199307ab7d1d6f9cadf2ad1365394c09ef3fbb96
Diamou Serigne Touba Ndiaye <dstndiaye@gmail.com>
2022-06-03 22:33:02 +0000

    ajoue des cards

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    center:true,
+    dots: true ,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
